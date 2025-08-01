aad423e74568b4fe75d0d2c1cdc09ee86b2fc803
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-08-01 08:47:24 +0530

    redis updated

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,6 +1,14 @@
 import { Redis } from '@upstash/redis'
 
 export const redis = new Redis({
-  url: 'https://creative-bass-44892.upstash.io',
-  token: 'Aa9cAAIjcDE2M2Q4NTZjOTNhMmE0MjM2YjQ5NmM0NmZhNjc4MTdiNnAxMA',
+  url: 'https://social-orca-61550.upstash.io',
+  token: 'AfBuAAIjcDE5ZmEyYWRiOGQ2ODM0MWQ4YWI3Mjk4MDQ1ZTM1Zjk5ZHAxMA',
 })
+
+//AfBuAAIjcDE5ZmEyYWRiOGQ2ODM0MWQ4YWI3Mjk4MDQ1ZTM1Zjk5ZHAxMA
+
+// pvs Aa9cAAIjcDE2M2Q4NTZjOTNhMmE0MjM2YjQ5NmM0NmZhNjc4MTdiNnAxMA
+
+// social-orca-61550.upstash.io
+
+// pvs creative-bass-44892.upstash.io
