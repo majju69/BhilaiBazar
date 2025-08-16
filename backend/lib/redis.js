13d9ab643bebf327b335c8cb55993f926a106f46
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-08-16 13:03:06 +0530

    updated redis

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,8 +1,8 @@
 import { Redis } from '@upstash/redis'
 
 export const redis = new Redis({
-  url: 'https://social-orca-61550.upstash.io',
-  token: 'AfBuAAIjcDE5ZmEyYWRiOGQ2ODM0MWQ4YWI3Mjk4MDQ1ZTM1Zjk5ZHAxMA',
+  url: 'https://immortal-crow-49588.upstash.io',
+  token: 'AcG0AAIncDFlMGFkYjYwOGE0OTM0OTk4OGE3NWMyZjlhYzE2NWE0N3AxNDk1ODg',
 })
 
 //AfBuAAIjcDE5ZmEyYWRiOGQ2ODM0MWQ4YWI3Mjk4MDQ1ZTM1Zjk5ZHAxMA
